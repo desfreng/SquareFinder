d32e51ecda4ff48ed7d241d7a7c8d3252fd5e332
Gabriel Desfrene <desfrene.gabriel@gmail.com>
2017-11-15 20:47:23 +0100

    Add Comments

--- widget.cpp
+++ widget.cpp
@@ -16,15 +16,16 @@ Widget::~Widget()
 
 void Widget::calculer()
 {
+    //On suprime notre liste et on initialise nos différentes variables
     ui->listWidget->clear();
-    //On récupère notre valeur de fin d'itération
+    
     unsigned long max = static_cast<unsigned long> (ui->finSpinBox->value());
     
     unsigned long u = 2, v = 0, a = 0, b = 0;
     
-    
     while (! (a > max || b > max)) {
     
+        //Si 'u' est pair, 'v' doit etre impair et inversement, puisque 'p' et 'u' n'ont pas la meme parité
         if (u % 2 == 0) {
             v = 1;
         }
@@ -35,19 +36,17 @@ void Widget::calculer()
         while (v < u) {
         
             if (pgcd (u, v) != 1) {
-            
+                //Si 'u' et 'v' ne sont pas premier entre eux on ajoute 2 à 'v'
                 v += 2;
             }
             else {
+                //Sinon on a trouvé un triplet pythagoricien irréductible (avec a = u²-v² et b = 2uv)
                 a = u * u - v * v;
                 b = 2 * u * v;
                 
-                //if (a > max || b > max) {
-                //    return;
-                //}
-                
+                //On l'ajoute à la liste
                 ui->listWidget->addItem (QString::number (a) + QString::fromUtf8 ("² + ") + QString::number (b) +  QString::fromUtf8 ("² = ") + QString::number (u * u + v * v) +  QString::fromUtf8 ("²"));
-                
+                //                                        a²                         +                       b²                          =                             c²
                 v += 2;
             }
         }
@@ -58,6 +57,8 @@ void Widget::calculer()
 
 unsigned long Widget::pgcd (unsigned long a, unsigned long b)
 {
+    //Calcul du PGCD
+    
     unsigned long r;
     
     while (b > 0) {
